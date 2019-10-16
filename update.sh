fb9bd7a5f5cfeb312ce5ee757c0cc56325792c2d
Aron <aronwc@gmail.com>
2019-10-16 09:40:20 -0500

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#git remote add template https://github.com/tapilab/iit-cs579/sample-project
+git remote add template https://github.com/iit-cs579/sample-project
 git fetch template
 git merge template/master --allow-unrelated-histories
 
